34ce13df66b7d9ca0c78fa4a6da33b6126e0fb9d
Fernando Gimeno <fernandogimeno.ief@gmail.com>
2023-09-06 17:26:36 -0300

    Añadido: tarjeta con informacion del usuario y refactorización

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+const container = document.getElementById("container");
+let userData;
+
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("form").addEventListener("submit", async (event) => {
     // Prevengo que se refresque la pagina
@@ -13,26 +16,54 @@ document.addEventListener("DOMContentLoaded", () => {
       date: date.value,
     };
 
-    try {
-      // Hago el llamado a la API pasandole un método POST y como tipo de contenido le digo que va a ser un JSON
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/users",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(formData), // Convierto el objeto formData en un JSON
-        }
-      );
-      if (!response.ok) {
-        throw new Error("Error en la llamada a la API");
-      }
-      const jsonData = await response.json(); // Convierto la respuesta a JSON
-      console.log(jsonData); // Muestro por consola la respuesta
-    } catch (error) {
-      // Si ocurre un error en la llamada a la API automáticamente cae en el catch y muestro el error
-      console.error(error);
-    }
+    // Esperamos a que se cree el usuario y lo asignamos a la variable global userData
+    userData = await createNewUser(formData);
+
+    // Mostramos el usuario creado
+    showUser(userData);
   });
 });
+
+async function createNewUser(user) {
+  try {
+    // Hago el llamado a la API pasandole un método POST y como tipo de contenido le digo que va a ser un JSON
+    const response = await fetch("https://jsonplaceholder.typicode.com/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user), // Convierto el objeto formData en un JSON
+    });
+    if (!response.ok) {
+      throw new Error("Error en la llamada a la API");
+    }
+    const data = await response.json(); // Convierto la respuesta a JSON
+    return data;
+  } catch (error) {
+    // Si ocurre un error en la llamada a la API automáticamente cae en el catch y muestro el error
+    console.error(error);
+  }
+}
+
+// Función utilizada para mostrar la fecha pasada por parámetro en formato día/mes/año
+function formatDate(date) {
+  const [year, month, day] = date.split("-");
+  return `${day}/${month}/${year}`;
+}
+
+// Definimos la función que crea una tarjeta con los datos del usuario que le pasamos por parámetro
+function showUser(user) {
+  container.innerHTML = `
+      <div class="card">
+        <h5 class="card-header">Usuario</h5>
+        <div class="card-body">
+          <p class="card-text"><strong>ID: </strong>${user.id}</p>
+          <p class="card-text"><strong>Nombre: </strong>${user.name}</p>
+          <p class="card-text"><strong>Apellido: </strong>${user.lastname}</p>
+          <p class="card-text"><strong>Fecha: </strong>${formatDate(
+            user.date
+          )}</p>
+        </div>
+      </div>
+    `;
+}
