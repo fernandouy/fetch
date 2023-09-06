982c4d06823b7e1c283a4af50fed2e3352383849
Fernando Gimeno <fernandogimeno.ief@gmail.com>
2023-09-06 16:20:33 -0300

    Mejoras y comentarios en el index.js

--- js/index.js
+++ js/index.js
@@ -1,18 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("form").addEventListener("submit", async (event) => {
+    // Prevengo que se refresque la pagina
     event.preventDefault();
 
-    const name = document.getElementById("name").value;
-    const lastname = document.getElementById("lastname").value;
-    const date = document.getElementById("date").value;
+    // Declaro variables a partir del indice del array target
+    const [name, lastname, date] = event.target;
 
-    const data = {
-      name,
-      lastname,
-      date,
+    // Asigno un objeto con los datos ingresados en los campos del formulario
+    const formData = {
+      name: name.value,
+      lastname: lastname.value,
+      date: date.value,
     };
 
     try {
+      // Hago el llamado a la API pasandole un método POST y como tipo de contenido le digo que va a ser un JSON
       const response = await fetch(
         "https://jsonplaceholder.typicode.com/users",
         {
@@ -20,13 +22,16 @@ document.addEventListener("DOMContentLoaded", () => {
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify(data),
+          body: JSON.stringify(formData), // Convierto el objeto formData en un JSON
         }
       );
-      const jsonData = await response.json();
-      console.log(jsonData);
-      return jsonData;
+      if (!response.ok) {
+        throw new Error("Error en la llamada a la API");
+      }
+      const jsonData = await response.json(); // Convierto la respuesta a JSON
+      console.log(jsonData); // Muestro por consola la respuesta
     } catch (error) {
+      // Si ocurre un error en la llamada a la API automáticamente cae en el catch y muestro el error
       console.error(error);
     }
   });
